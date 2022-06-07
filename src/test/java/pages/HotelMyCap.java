40c1d215bf076c08f1cb4546811b67b46672da12
Ahmet Kaya <ahka1143@gmail.com>
2022-06-07 10:18:36 +0300

    7.06.2022

---
+++ src/test/java/pages/HotelMyCap.java
@@ -0,0 +1,80 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import utilities.Driver;
+
+import java.util.List;
+
+public class HotelMyCap {
+    public HotelMyCap() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//*[text()='Home']")
+    public WebElement homeLink;
+
+    @FindBy(xpath = "//*[text()='Rooms']")
+    public WebElement roomsLink;
+
+    @FindBy(xpath = "//*[text()='Restaurant']")
+    public WebElement restaurantLink;
+
+    @FindBy(xpath = "//*[text()='About']")
+    public WebElement aboutLink;
+
+    @FindBy(xpath = "//*[text()='Blog']")
+    public WebElement blogLink;
+
+    @FindBy(xpath = "//*[text()='Contact']")
+    public WebElement contactLink;
+
+    @FindBy(xpath = "//*[text()='Log in']")
+    public WebElement loginLink;
+
+
+    @FindBy(tagName = "h1")
+    public WebElement ourRestourantYazisi;
+
+    @FindBy(xpath = "//div[@style='background-image: url(/siteTemplate/images/about.jpg);']")
+    public WebElement solTaraftakiResim;
+
+    @FindBy (xpath = "//div[@class='col-md-7 py-5 wrap-about pb-md-5 ftco-animate fadeInUp ftco-animated']")
+    public  WebElement resminSagindakiAciklama;
+
+    @FindBy (id = "navRestaurant")
+    public WebElement restourantSayfasiLinki;
+
+    @FindBy (xpath = "//div[@class='col-md-6']")
+    public List<WebElement> restourantMenuSutunList;
+
+    @FindBy(xpath = "(//div[@class='col-md-6'])[1]//h3")
+    public List<WebElement> restourantMenuBirinciSutunList;
+
+    @FindBy(xpath = "(//div[@class='col-md-6'])[1]//h3")
+    public List<WebElement> restourantMenuIkinciSutunList;
+
+    @FindBy(xpath = "(//h3)[6]")
+    public WebElement overLoadElementi;
+
+    @FindBy(xpath = "(//h3)[8]")
+    public WebElement hamPineAppleElementi;
+
+    @FindBy(xpath = "(//h3)[5]")
+    public WebElement grilledBeefElementi;
+
+    @FindBy(xpath = "//div[@class='col-md-12 ftco-animate fadeInUp ftco-animated']")
+    public WebElement yemekResimleriSlideri;
+
+    @FindBy(xpath = "//button[@class='owl-prev']")
+    public WebElement yemekSlideriGeriButonu;
+
+    @FindBy(xpath = "//button[@class='owl-next']")
+    public WebElement yemekSlideriIleriButonu;
+
+    @FindBy(xpath = "//button[@role='button']")
+    public List<WebElement> yemekResimleriSecimButonlariList;
+}
---
+++ src/test/java/stepDefinitions/HotelMyCapStepDefinitions.java
@@ -0,0 +1,132 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import pages.HotelMyCap;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class HotelMyCapStepDefinitions {
+    HotelMyCap hotelMyCap = new HotelMyCap();
+
+    @Given("Kullanici HotelMyCap website gider")
+    public void kullanici_hotel_my_cap_website_gider() {
+        Driver.getDriver().get(ConfigReader.getProperty("hotelMyCapUrl"));
+    }
+
+    @Given("Home linkinin visibled and enabled oldugunu gorur")
+    public void home_linkinin_visibled_and_enabled_oldugunu_gorur() {
+        Assert.assertTrue(hotelMyCap.homeLink.isDisplayed());
+        Assert.assertTrue(hotelMyCap.homeLink.isEnabled());
+    }
+
+    @Given("Rooms linkinin visibled and enabled oldugunu gorur")
+    public void rooms_linkinin_visibled_and_enabled_oldugunu_gorur() {
+        Assert.assertTrue(hotelMyCap.roomsLink.isDisplayed());
+        Assert.assertTrue(hotelMyCap.roomsLink.isEnabled());
+    }
+
+    @Given("Resturant linkinin visibled and enabled oldugunu gorur")
+    public void resturant_linkinin_visibled_and_enabled_oldugunu_gorur() {
+        Assert.assertTrue(hotelMyCap.restaurantLink.isDisplayed());
+        Assert.assertTrue(hotelMyCap.restaurantLink.isEnabled());
+    }
+
+    @Given("About linkinin visibled and enabled oldugunu gorur")
+    public void about_linkinin_visibled_and_enabled_oldugunu_gorur() {
+        Assert.assertTrue(hotelMyCap.aboutLink.isDisplayed());
+        Assert.assertTrue(hotelMyCap.aboutLink.isEnabled());
+    }
+
+    @Given("Blog linkinin visibled and enabled oldugunu gorur")
+    public void blog_linkinin_visibled_and_enabled_oldugunu_gorur() {
+        Assert.assertTrue(hotelMyCap.blogLink.isDisplayed());
+        Assert.assertTrue(hotelMyCap.blogLink.isEnabled());
+    }
+
+    @Given("Contact linkinin visibled and enabled oldugunu gorur")
+    public void contact_linkinin_visibled_and_enabled_oldugunu_gorur() {
+        Assert.assertTrue(hotelMyCap.contactLink.isDisplayed());
+        Assert.assertTrue(hotelMyCap.contactLink.isEnabled());
+    }
+
+    @Given("Login linkinin visibled and enabled oldugunu gorur")
+    public void login_linkinin_visibled_and_enabled_oldugunu_gorur() {
+        Assert.assertTrue(hotelMyCap.loginLink.isDisplayed());
+        Assert.assertTrue(hotelMyCap.loginLink.isEnabled());
+    }
+
+    @And("“OurRestaurant” basliginin gorunur oldugu test edilir")
+    public void ourrestaurantBasligininGorunurOlduguTestEdilir() {
+
+        Assert.assertTrue(hotelMyCap.ourRestourantYazisi.isDisplayed());
+
+    }
+
+    @And("Sol tarafta resmin gorunur oldugu test edilir")
+    public void solTaraftaResminGorunurOlduguTestEdilir() {
+        Assert.assertTrue(hotelMyCap.solTaraftakiResim.isDisplayed());
+
+    }
+
+    @And("Resmin saginda aciklamalar bolumunun yer aldigi test edilir")
+    public void resminSagindaAciklamalarBolumununYerAldigiTestEdilir() {
+        Assert.assertTrue(hotelMyCap.resminSagindakiAciklama.isDisplayed());
+
+    }
+
+    @And("Restourant sayfasina gider")
+    public void restourantSayfasinaGider() {
+        hotelMyCap.restourantSayfasiLinki.click();
+
+    }
+
+    @And("Menu altinda iki sutun bulundugunu ve her sutun altinda dorder element oldugu test edilir")
+    public void menuAltindaIkiSutunBulundugunuVeHerSutunAltindaDorderElementOlduguTestEdilir() {
+
+        Assert.assertEquals(2, hotelMyCap.restourantMenuSutunList.size());
+        Assert.assertEquals(4, hotelMyCap.restourantMenuBirinciSutunList.size());
+        Assert.assertEquals(4, hotelMyCap.restourantMenuIkinciSutunList.size());
+
+
+    }
+
+    @And("“Ultimate Overload” altinda “Ham & Pineapple” ustunde “Grilled Beef with potatoes” oldugu test edilir")
+    public void ultimateOverloadAltindaHamPineappleUstundeGrilledBeefWithPotatoesOlduguTestEdilir() {
+        int overLoadElementiYeri = hotelMyCap.overLoadElementi.getLocation().getY();
+        int hamAppleElementiYeri = hotelMyCap.hamPineAppleElementi.getLocation().getY();
+        int grilledBeefElementiYeri = hotelMyCap.grilledBeefElementi.getLocation().getY();
+        Assert.assertTrue(overLoadElementiYeri < hamAppleElementiYeri && overLoadElementiYeri > grilledBeefElementiYeri);
+
+
+    }
+
+    @And("Yemek resimleri sliderinin gorundugu test edilir")
+    public void yemekResimleriSliderininGorunduguTestEdilir() {
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        js.executeScript("arguments[0].scrollIntoView(true);",hotelMyCap.yemekResimleriSecimButonlariList.get(0));
+        Assert.assertTrue(hotelMyCap.yemekResimleriSlideri.isDisplayed());
+    }
+
+    @And("sag ve sol yon tuslarinin enable oldugu test edilir")
+    public void sagVeSolYonTuslarininEnableOlduguTestEdilir() {
+        Assert.assertTrue(hotelMyCap.yemekSlideriGeriButonu.isEnabled());
+        Assert.assertTrue(hotelMyCap.yemekSlideriIleriButonu.isEnabled());
+    }
+
+    @And("resimlerin altinda dort adet butonun enable oldugu test edilir")
+    public void resimlerinAltindaDortAdetButonunEnableOlduguTestEdilir() {
+        hotelMyCap.yemekResimleriSecimButonlariList.stream().forEach(t->Assert.assertTrue(t.isEnabled()));
+    }
+
+    @Then("sayfayi kapatir")
+    public void sayfayiKapatir() {
+        Driver.closeDriver();
+    }
+}
